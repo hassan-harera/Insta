135d0ce7997dbe593f8e358afe8bdf7e1a60341b
Harera <hassanstar201118@gmail.com>
2021-08-30 00:58:09 +0200

    HomeActivity using compose navigation

---
+++ app/src/main/java/com/whiteside/insta/ui/home/HomeActivity.kt
@@ -0,0 +1,180 @@
+package com.whiteside.insta.ui.home
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil.annotation.ExperimentalCoilApi
+import com.example.compose1.ui.theme.Grey660
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.whiteside.insta.ui.chats.ChatViewModel
+import com.whiteside.insta.ui.chats.HomeChats
+import com.whiteside.insta.ui.components.HomeTopBar
+import com.whiteside.insta.ui.feed.HomeFeed
+import com.whiteside.insta.ui.feed.FeedViewModel
+import com.whiteside.insta.ui.utils.Arguments.POST_ID
+import com.whiteside.insta.ui.utils.Arguments.UID
+import com.whiteside.insta.ui.home.HomeNavigation.AddPost
+import com.whiteside.insta.ui.home.HomeNavigation.PostScreen
+import com.whiteside.insta.ui.home.HomeNavigation.VisitProfile
+import com.whiteside.insta.ui.homeprofile.HomeProfile
+import com.whiteside.insta.ui.homeprofile.HomeProfileViewModel
+import com.whiteside.insta.ui.notifications.HomeNotifications
+import com.whiteside.insta.ui.postform.PostForm
+import com.whiteside.insta.ui.viewpost.PostScreen
+import com.whiteside.insta.ui.viewpost.PostViewModel
+import com.whiteside.insta.ui.visitprofile.VisitProfile
+import com.whiteside.insta.ui.visitprofile.VisitProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterialApi::class)
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity() {
+
+    private val feedViewModel: FeedViewModel by viewModels()
+    private val homeProfileViewModel: HomeProfileViewModel by viewModels()
+    private val postViewModel: PostViewModel by viewModels()
+    private val visitProfileViewModel: VisitProfileViewModel by viewModels()
+    private val chatViewModel: ChatViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_UNSPECIFIED);
+
+        setContent {
+            HomeActivityContent()
+        }
+    }
+
+    @Composable
+    private fun HomeActivityContent() {
+        val bottomNavIcons = listOf(
+            HomeBottomNavigation.Feed,
+            HomeBottomNavigation.Profile,
+            HomeBottomNavigation.Notifications,
+            HomeBottomNavigation.Chats
+        )
+
+        val navController = rememberNavController()
+
+        Scaffold(
+            topBar = {
+                HomeTopBar()
+            },
+            bottomBar = {
+                HomeBottomNavigation(tabs = bottomNavIcons, navController = navController)
+            }
+        ) {
+            HomeNavHost(navController = navController, it)
+        }
+    }
+
+    @ExperimentalCoilApi
+    @Composable
+    private fun HomeNavHost(navController: NavHostController, paddingValues: PaddingValues) {
+        NavHost(
+            startDestination = HomeNavigation.HomeFeed,
+            navController = navController,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            composable(HomeNavigation.HomeFeed) {
+                HomeFeed(
+                    feedViewModel = feedViewModel,
+                    navController = navController,
+                    postViewModel = postViewModel
+                )
+            }
+            composable(HomeNavigation.HomeProfile) {
+                HomeProfile(
+                    homeProfileViewModel = homeProfileViewModel,
+                    postViewModel = postViewModel,
+                    navController = navController
+                )
+            }
+            composable(HomeNavigation.HomeNotifications) {
+                HomeNotifications()
+            }
+            composable(HomeNavigation.HomeChats) {
+                HomeChats(chatViewModel = chatViewModel)
+            }
+            composable("$VisitProfile/{$UID}") { stack ->
+                VisitProfile(
+                    visitProfileViewModel = visitProfileViewModel,
+                    navController = navController,
+                    postViewModel = postViewModel,
+                    uid = stack.arguments!!.getString(UID)!!
+                )
+            }
+            composable("$PostScreen/{$POST_ID}") { stack ->
+                PostScreen(
+                    postViewModel,
+                    stack.arguments!!.getString(POST_ID)!!,
+                    navController
+                )
+            }
+            composable(AddPost) { stack ->
+                PostForm(
+                    navController,
+                    postViewModel
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun HomeBottomNavigation(
+        tabs: List<HomeBottomNavigation>,
+        navController: NavHostController
+    ) {
+        BottomNavigation(
+            modifier = Modifier
+                .fillMaxWidth(),
+            backgroundColor = Grey660,
+            contentColor = Color.Unspecified,
+            elevation = 0.dp,
+        ) {
+            tabs.forEachIndexed { index, tab ->
+                IconButton(
+                    onClick = {
+                        navController.navigate(tabs[index].route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    modifier = Modifier.padding(12.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = tab.icon),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/whiteside/insta/ui/home/HomeNavigation.kt
@@ -0,0 +1,11 @@
+package com.whiteside.insta.ui.home
+
+object HomeNavigation {
+    val HomeFeed = "HomeFeed"
+    val HomeProfile = "HomeProfile"
+    val HomeChats = "HomeChats"
+    val HomeNotifications = "HomeNotifications"
+    val VisitProfile = "VisitProfile"
+    val PostScreen = "PostScreen"
+    val AddPost = "AddPost"
+}
