cadf1a56de5e465cf024f2971ac87528269aa5ef
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-09-02 08:52:11 +0200

    App Theme.kt

---
+++ app/src/main/java/com/whiteside/insta/ui/theme/String.kt
@@ -0,0 +1,10 @@
+package com.whiteside.insta.ui.theme
+
+
+val like = "Like"
+val addImage = "Add image"
+val Image = "Image"
+val commment = "Comment"
+val attachment = "Attachment"
+val share = "Share"
+val post = "Post"
