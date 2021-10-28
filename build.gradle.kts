a5a23c7d27c345d4fbc00ff55c27f5b6284fca45
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-28 22:33:03 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath(Libs.navigationSafeArgsPlugin)
         classpath(Libs.kotlinPlugin)
         classpath(Libs.googleServices)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     }
 }
 
