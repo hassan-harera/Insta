1f32f999bde872650fa84eaf253f3b50bd08daa0
hassan shaban <61611892+hassan0shaban@users.noreply.github.com>
2021-11-08 23:32:46 +0200

    check token in SP

--- app/src/main/java/com/harera/insta/MainActivity.kt
+++ app/src/main/java/com/harera/insta/MainActivity.kt
@@ -2,34 +2,39 @@ package com.harera.insta
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import coil.annotation.ExperimentalCoilApi
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
-import com.google.firebase.auth.FirebaseUser
-import com.harera.repository.db.network.abstract_.AuthManager
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.harera.base.base.BaseViewModel
+import com.harera.base.datastore.UserSharedPreferences
+import com.harera.base.state.State
+import com.harera.home.HomeActivity
+import com.harera.login.LoginActivity
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 
+@ExperimentalPagerApi
+@ExperimentalMaterialApi
 @ExperimentalCoilApi
 @ExperimentalComposeUiApi
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,63 +46,52 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun SplashScreen(
-        mainViewModel: MainViewModel = getViewModel()
+        mainViewModel: MainViewModel = getViewModel(),
     ) {
         val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.splash))
         val progress by animateLottieCompositionAsState(composition)
-        val user by mainViewModel.user
-        val context = LocalContext.current
-
-        LaunchedEffect(user) {
-            if (user == null)
-                //TODO replace with login
-                mainViewModel.signIn()
-        }
+        val token = mainViewModel.token
+        Log.d(TAG, "SplashScreen: $token")
 
         Box {
-            LottieAnimation(
-                composition,
-                progress,
-            )
+            LottieAnimation(composition, progress)
 
             Text(
                 text = "Skip intro",
                 modifier = Modifier
                     .padding(20.dp)
                     .clickable {
-                        startActivity(Intent(context, HomeActivity::class.java))
-                        finish()
+                        checkUser(token)
                     }
             )
 
-            if (progress >= 1f && user != null) {
-                startActivity(Intent(context, HomeActivity::class.java))
-                finish()
+            if (progress >= 0.9) {
+                checkUser(token)
             }
+        }
+    }
+
+    private fun checkUser(token: String?) {
+        if (token.isNullOrBlank()) {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    LoginActivity::class.java
+                )
+            )
+            finish()
+        } else {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    HomeActivity::class.java
+                )
+            )
+            finish()
         }
     }
 }
 
-
 class MainViewModel constructor(
-    private val authManager: AuthManager,
-) : ViewModel() {
-    var user = mutableStateOf<FirebaseUser?>(null)
-
-    init {
-        getCurrentUser()
-    }
-
-    private fun getCurrentUser() {
-        user.value = authManager.getCurrentUser()
-    }
-
-    suspend fun signIn() {
-        authManager.signInWithEmailAndRandomPassword(
-            "hassan.shaban.harera@gmail.com"
-        )
-            .addOnSuccessListener {
-                user.value = it.user
-            }
-    }
-}
+    userPreferences: UserSharedPreferences,
+) : BaseViewModel<State>(userSharedPreferences = userPreferences)
